e4dd030bf41d27ee36a8ffb456b337d41da7e13d
Hassanjavaid <hassanjavaid1214@gmail.com.com>
2025-03-26 00:32:57 +0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 import withPWA from "next-pwa";
 
 const nextConfig = {
-  distDir: "build",
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
@@ -22,17 +21,4 @@ export default withPWA({
   disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
   register: true, // register the PWA service worker
   skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: "http://localhost:3000/api/songs", // Adjust for your API URL
-      handler: "CacheFirst",
-      options: {
-        cacheName: "audio-cache",
-        expiration: {
-          maxEntries: 100, // Store up to 20 songs
-          maxAgeSeconds: 60 * 60 * 24 * 7, // Cache for 7 days
-        },
-      },
-    },
-  ],
 })(nextConfig);
